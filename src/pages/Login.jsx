940db5c783dead091216ac4d5ef25abe882b2232
Muhammadali-fr <boburjonr934@gmail.com>
2025-06-04 14:39:47 +0500

    login page input malumotlarini backendga jonatish

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,51 +8,54 @@ import { login } from "../request";
 import { useAppStore } from "../lib/zustand";
 import { useLocation, useNavigate } from "react-router-dom";
 
+// react-router-dom 
+import { Link } from "react-router-dom";
+
 function Login() {
+  // userning inputdan olingan malumoti
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const setUser = useAppStore((state) => state.setUser);
-  const navigate = useNavigate();
 
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
+  // input malumotlarini saqlash uchun
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
 
-  async function handleSubmit(e) {
+  // login funksiya 
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = getFormData(e.target);
     setIsLoading(true);
 
     try {
-      const res = await login(result);
-
-      if (!res || res.status === "error" || !res.refreshtoken) {
-        toast.error(res?.message || "Login muvaffaqiyatsiz");
-        return;
-      }
-
-      toast.success("Xush kelibsiz!");
-      navigate("/");
-      setUser(res || {});
-      localStorage.setItem("refreshToken", res.refreshtoken);
-
-      setTimeout(() => {
-        navigate(from, { replace: true });
-      }, 500);
-    } catch (err) {
-      toast.error(err.message || "Loginda xatolik");
-    } finally {
-      setIsLoading(false);
+      const res = await login(formData);
+      console.log(res, "user muvafaqqiyatli login qildi!!!");
+      // refresh tokenni localstoragega saqlsh 
+      localStorage.setItem("refreshToken", res.refreshToken)
     }
-  }
+    catch (error) {
+      console.log("login err", error.message);
+      toast.error(error.message || "Nimadir xato ketti qaytadan urinib koring.");
+    }
+    finally { setIsLoading(false) };
+  };
 
   return (
     <div className="flex h-screen items-center bg-white">
+      {/* right image */}
       <img
         src={signin}
         className="h-full w-[50%] bg-[#C9AC8CED] px-[50px] py-[101px]"
         alt="Sign In Illustration"
       />
 
+      {/* left form  */}
       <div className="mx-auto flex flex-col bg-white text-black">
         <form onSubmit={handleSubmit}>
           <h2 className="font-slab mb-5 text-left text-[40px] font-black">
@@ -60,29 +63,37 @@ function Login() {
           </h2>
 
           <p className="mb-[21px] mt-[5px] font-sans text-[13px] font-normal">
-            Already have an account?{" "}
-            <a className="text-blue-600 underline" href="/register">
+            Already have an account?
+            <Link className="text-blue-600 underline" to="/register">
               Register
-            </a>
+            </Link>
           </p>
 
           <div className="flex flex-col gap-5">
             <div className="flex flex-col gap-4">
+
+              {/* email  */}
               <input
                 className="w-50% rounded-xl border border-[#474747] px-[29px] py-[16px]"
                 name="email"
                 type="email"
                 placeholder="Email"
                 required
+                value={formData.email}
+                onChange={handleChange}
               />
 
               <div className="relative">
+
+                {/* password  */}
                 <input
                   className="w-[430px] rounded-xl border border-[#474747] px-[29px] py-[16px] pr-12"
                   name="password"
                   type={showPassword ? "text" : "password"}
                   placeholder="Password"
                   required
+                  value={formData.password}
+                  onChange={handleChange}
                 />
                 <button
                   type="button"
